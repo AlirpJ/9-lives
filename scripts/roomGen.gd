b78194364dd5a9e10f780680c7a817d7e5b41830
AlirpJ <prila.jsp@gmail.com>
2025-08-01 12:45:31 -0500

    Add room generation script

--- scripts/roomGen.gd
+++ scripts/roomGen.gd
@@ -8,6 +8,7 @@ var availableRooms = []
 var steps = 0
 var goal = 99999
 var popped
+var z = 0
 
 var path0
 var path1 = -1
@@ -17,16 +18,24 @@ var path4 = -1
 
 var persistentRooms = []
 
+var rooms = {
+# tileID 	: paths
+0			:	[-1,-1,-1,-1],
+1			:	[-1,-1],
+2			:	[-1,-1,-1],
+201			:	[-1,-1]
+}
+
 # roomGen is called when you click to enter a new room
 func roomGen(x,y): #roomGen(x,y) takes x: last room ID, and y: new room ID
 	
-	#z = getPathsFromId(y) # Get the number of exits to a room given its ID
+	z = getPathsFromId(y).length() # Get the number of exits to a room given its ID
 	path0 = x # path0 will ALWAYS take you back where you came
 	
 	# Check for resource
 	if Game.resource < 1:
 		pass
-		# die function here
+		# Lose 1 life
 	
 	# Check for victory
 	if getRoomType(y) == "victory":
@@ -35,43 +44,51 @@ func roomGen(x,y): #roomGen(x,y) takes x: last room ID, and y: new room ID
 	
 	# Loop Logic
 	if getRoomType(y) == "loop":
-		match pickRandomPath(1):
-			1:
-				path1 = 0
-			2:
-				path2 = 0
-			3:
-				path3 = 0
-			4: 
-				path4 = 0
+		for a in getRoom(y):
+			a=-1
+		getRoom(y).set(z,0)
 		# randomly assign non-progress rooms
-	
+		for a in getRoom(y):
+			if a != 0:
+				a = randomNonProgress(z)
+				popper(a)
+				
 	# Dead End? Turn Back!
 	if getRoomType(y) == "deadEnd":
 		path0 = x
-		path1 = -1
-		path2 = -1
-		path3 = -1
-		path4 = -1
 	
 	# Progress Logic
 	if getRoomType(y) == "progress":
 		# randomly assign non-progress rooms
-		pass 
+		for a in getRoom(y):
+			if a != 0:
+				a = randomNonProgress(z) 
+				popper(a)
 	
 	# Corridor logic
 	if getRoomType(y) == "corridor":
+		for a in getRoom(y):
+			a = -1
 		if steps == goal:
+			var randRoom = randomProgressRoom(z)
+			var assignMe = pickRandomPath(z)
 			# randomly assign a progress room
-			pass
+			getRoom(y).set(assignMe,randRoom)
+			popper(randRoom)
 		else:
 			# randomly assign non-progress rooms
-			pass
+			for a in getRoom(y):
+				if a != 0:
+					a = randomNonProgress(z)
+					popper(a)
 
 
 
 
 ### HELPER FUNCTIONS ###
+
+func getRoom(roomId):
+	return get(rooms)
 
 func getRoomType(roomId):
 	# Rooms:
@@ -95,6 +112,9 @@ func getRoomType(roomId):
 		return "victory"
 	else: 
 		return "broken"
+
+func getPathsFromId(y):
+	return get(rooms.get(y))
 
 func popper(removeMe):
 	popped = removeMe
@@ -128,3 +148,16 @@ func pickRandomPath(max):
 	randomPath = rng.randi_range(0,max)
 	return randomPath
 	
+func randomNonProgress(max):
+	var path = -1
+	# If room is not available and progress, try again
+	while !availableRooms.has(path) and getRoomType(path) == "progress":
+		path = pickRandomPath(max)
+	return path
+
+func randomProgressRoom(max):
+	var path = -1
+	# If room is not available and progress, try again
+	while !availableRooms.has(path) and getRoomType(path) != "progress":
+		path = pickRandomPath(max)
+	return path
