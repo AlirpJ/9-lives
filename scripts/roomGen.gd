5ae0e2d917dabd4952a4080f18a1dbe3c5a124bf
AlirpJ <prila.jsp@gmail.com>
2025-08-02 02:26:32 -0500

    minor fixes

--- scripts/roomGen.gd
+++ scripts/roomGen.gd
@@ -4,11 +4,14 @@ var rng = RandomNumberGenerator.new()
 var randomNum = -1
 var randomPath = -1
 
-var availableRooms = []
+var availableRooms = [0,1,2,3,4,5,6,101,104,201,202,203,204,301,302,303,304,999]
 var steps = 0
 var goal = 99999
 var popped
 var z = 0
+
+var newGame = true
+var currentRoom = 0
 
 var path0
 var path1 = -1
@@ -20,22 +23,69 @@ var persistentRooms = []
 
 var rooms = {
 # tileID 	: paths
-0			:	[-1,-1,-1,-1],
+0			:	[1,2,3,4],
 1			:	[-1,-1],
 2			:	[-1,-1,-1],
-201			:	[-1,-1]
+3			:	[-1,-1],
+4			:	[-1,-1,-1,-1],
+5			:	[-1,-1,-1,-1],
+6			:	[-1,-1,-1,-1],
+101			:	[-1,-1],
+102			:	[-1,-1,-1],
+103			:	[-1,-1],
+104			:	[-1,-1,-1,-1],
+201			:	[-1,-1],
+202			:	[-1,-1,-1],
+203			:	[-1,-1],
+204			:	[-1,-1,-1,-1],
+301			:	[-1,-1],
+302			:	[-1,-1,-1],
+303			:	[-1,-1],
+304			:	[-1,-1,-1,-1],
+999			:	[-1]
 }
+	
+# Rooms:
+# 0 = Start Room
+# 1-99 = Corridor Rooms
+# 100-199 = Progress Rooms
+# 200-299 = Loop Rooms
+# 300-399 = Dead Ends
+# 999 = Exit/Victory Room
 
 # roomGen is called when you click to enter a new room
+func _process(delta):
+	if Input.is_action_just_pressed("jump"):
+		
+		if newGame:
+			#print(currentRoom)
+			roomGen(-1,currentRoom)
+			#print(currentRoom,", ",getRoom(currentRoom)[0])
+			#currentRoom = rooms[currentRoom][0] #take the first path out of start
+			newGame = false
+		else:
+			#print("---")
+			#print(currentRoom,", ",getRoom(currentRoom)[0])
+			#print("ROOM GENERATION")
+			roomGen(currentRoom,getRoom(currentRoom)[0]) #take the first path out of your room
+			#print(getRoom(currentRoom)[0])
+			currentRoom = getRoom(currentRoom)[0] 
+			print(currentRoom,", ",getRoom(currentRoom)[0])
+			print(rooms.get(getRoom(currentRoom)[0]))
 func roomGen(x,y): #roomGen(x,y) takes x: last room ID, and y: new room ID
+	print("Left room ",x,"! Now in room ",y,".")
 	
-	z = getPathsFromId(y).length() # Get the number of exits to a room given its ID
+	z = rooms.get(y).size() # Get the number of exits to a room given its ID
 	path0 = x # path0 will ALWAYS take you back where you came
-	
+	print("z = ",z)
 	# Check for resource
 	if Game.resource < 1:
 		pass
 		# Lose 1 life
+	
+	if y == 0:
+		rooms[0] = [1,2,3,4]
+		
 	
 	# Check for victory
 	if getRoomType(y) == "victory":
@@ -67,8 +117,8 @@ func roomGen(x,y): #roomGen(x,y) takes x: last room ID, and y: new room ID
 	
 	# Corridor logic
 	if getRoomType(y) == "corridor":
-		for a in getRoom(y):
-			a = -1
+		#for a in getRoom(y):
+			#a = -1
 		if steps == goal:
 			var randRoom = randomProgressRoom(z)
 			var assignMe = pickRandomPath(z)
@@ -76,11 +126,20 @@ func roomGen(x,y): #roomGen(x,y) takes x: last room ID, and y: new room ID
 			getRoom(y).set(assignMe,randRoom)
 			popper(randRoom)
 		else:
+			print(getRoom(y))
 			# randomly assign non-progress rooms
-			for a in getRoom(y):
-				if a != 0:
-					a = randomNonProgress(z)
+			for a in range(len(getRoom(y))):
+				
+				if getRoom(y)[a] != 0:
+					
+					getRoom(y)[a] = randomNonProgress(z)
+					#print("lol ",a)
+					
 					popper(a)
+					
+					
+		print(getRoom(y))
+	print("Left room ",x,"! Now in room ",y,".")
 
 
 
@@ -88,7 +147,7 @@ func roomGen(x,y): #roomGen(x,y) takes x: last room ID, and y: new room ID
 ### HELPER FUNCTIONS ###
 
 func getRoom(roomId):
-	return get(rooms)
+	return rooms.get(roomId)
 
 func getRoomType(roomId):
 	# Rooms:
@@ -114,7 +173,7 @@ func getRoomType(roomId):
 		return "broken"
 
 func getPathsFromId(y):
-	return get(rooms.get(y))
+	return (rooms.get(y))
 
 func popper(removeMe):
 	popped = removeMe
@@ -145,19 +204,23 @@ func randomProgress():
 
 func pickRandomPath(max):
 	randomize()
-	randomPath = rng.randi_range(0,max)
+	randomPath = -1
+	while !availableRooms.has(randomPath):
+		randomPath = rng.randi_range(100,199)
 	return randomPath
 	
 func randomNonProgress(max):
-	var path = -1
-	# If room is not available and progress, try again
-	while !availableRooms.has(path) and getRoomType(path) == "progress":
-		path = pickRandomPath(max)
+	var path = max
+	# If room is not available and progress, pick a new random
+	while !availableRooms.has(path) and getRoomType(path) != "progress":
+		path = rng.randi_range(0,500)
+		print("path: ",path)
 	return path
 
 func randomProgressRoom(max):
 	var path = -1
-	# If room is not available and progress, try again
+	# If room is not available and not progress, pick a new random
 	while !availableRooms.has(path) and getRoomType(path) != "progress":
 		path = pickRandomPath(max)
+		print("path: ",path)
 	return path
