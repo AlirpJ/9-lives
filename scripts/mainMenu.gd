e5bb5548412367b8b220290402f7da14815698c7
Koichiw <wzy000201@gmail.com>
2025-08-03 02:26:15 -0500

    Created AudioManager and added sounds
    Created Audiomanager and organized sound assets. added UI sound and transition sounds to the start menu script. added bgm to the transition script and movement sound to the playerCharacter script.

--- scripts/mainMenu.gd
+++ scripts/mainMenu.gd
@@ -1,14 +1,24 @@
 extends Control
 
 func _on_new_game_pressed():
+	AudioManager.ui.play()
+	await AudioManager.ui.finished
 	SceneTransition.change_scene_to_file("res://scenes/startRoom.tscn")
+	AudioManager.chirp.play()
+	AudioManager.falling.play()
+	await AudioManager.chirp.finished
+	await AudioManager.falling.finished
 	Game.new = true
 	#Game.state = 0
 
 func _on_quit_pressed():
+	AudioManager.ui.play()
+	await AudioManager.ui.finished
 	get_tree().quit()
 
 func _on_resume_pressed():
+	AudioManager.ui.play()
+	await AudioManager.ui.finished
 	#var lastScene = "res://scenes/" + (str(Game.loadGame()['room'])) + ".tscn"
 	#SceneTransition.change_scene_to_file(lastScene)
 	pass
--- scripts/player_character.gd
+++ scripts/player_character.gd
@@ -44,8 +44,10 @@ func _physics_process(delta: float) -> void:
 	if moving:
 		global_position = lerp(global_position, mv_endpoint, SPEED)
 		look_at(-mv_endpoint)
+		AudioManager.movement.play()
 		if global_position == mv_endpoint:
 			moving = false
+			AudioManager.movement.stop()
 
 # INFO: Connects to movement_arrow signal.
 func _mv_arrow_selected(endpoint: Vector3) -> void:
